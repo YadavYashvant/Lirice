5004a5efdb9b32f9d14683eab463f256b564f18a
yashvant <yashvantyadav855@gmail.com>
2024-04-10 23:25:36 +0530

    Initial commit

---
+++ src/main/java/com/yashvant/lirice/LiriceApplication.java
@@ -0,0 +1,13 @@
+package com.yashvant.lirice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiriceApplication.class, args);
+    }
+
+}
