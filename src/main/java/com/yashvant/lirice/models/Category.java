245364f5b884531d1541cb63fdafd0c5c4a31795
yashvant <yashvantyadav855@gmail.com>
2024-04-11 23:25:02 +0530

    making models

--- src/main/java/com/yashvant/lirice/models/Category.java
+++ src/main/java/com/yashvant/lirice/models/Category.java
@@ -1,4 +1,23 @@
 package com.yashvant.lirice.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "CATEGORY")
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Category {
+
+    @Id
+    @Column(name = "category_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
 }
--- src/main/java/com/yashvant/lirice/models/Product.java
+++ src/main/java/com/yashvant/lirice/models/Product.java
@@ -1,4 +1,37 @@
 package com.yashvant.lirice.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "PRODUCT")
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Product {
+
+    @Id
+    @Column(name = "product_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+
+    private String image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private Category category;
+
+    private int quantity;
+
+    private int price;
+
+    private int weight;
+
+    private String description;
 }
