e99c9fdffd0755b59830f471b355ba4cc9526436
YadavYashvant <yashvantyadav855@gmail.com>
2024-05-02 22:28:44 +0530

    fixing background

--- client/lirice-client/src/app/page.tsx
+++ client/lirice-client/src/app/page.tsx
@@ -3,18 +3,53 @@ import { HeroHighlight } from "@/components/ui/hero-highlight";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import { useState } from "react";
+import { useMotionValue, motion, useMotionTemplate } from "framer-motion";
 import { Button, Card, NextUIProvider } from "@nextui-org/react";MenuItem
 import Image from "next/image";
 export default function Home() {
+
+  let mouseX = useMotionValue(0);
+  let mouseY = useMotionValue(0);
+
+  function handleMouseMove({
+    currentTarget,
+    clientX,
+    clientY,
+  }: React.MouseEvent<HTMLDivElement>) {
+    if (!currentTarget) return;
+    let { left, top } = currentTarget.getBoundingClientRect();
+
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+  }
+
   return (
     <NextUIProvider>
-      <div className="p-5">
+      <div
+      className="relative h-[100vh] flex items-center bg-white dark:bg-black justify-center w-full group"
+      onMouseMove={handleMouseMove}
+    >
         <Navbar/>
-        <HeroHighlight>
-          <Card>
 
-          </Card>
-        </HeroHighlight>
+        <motion.div
+        className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
+        style={{
+          WebkitMaskImage: useMotionTemplate`
+            radial-gradient(
+              200px circle at ${mouseX}px ${mouseY}px,
+              black 0%,
+              transparent 100%
+            )
+          `,
+          maskImage: useMotionTemplate`
+            radial-gradient(
+              200px circle at ${mouseX}px ${mouseY}px,
+              black 0%,
+              transparent 100%
+            )
+          `,
+        }}
+      />
       </div>
     </NextUIProvider>
   );
@@ -40,25 +75,21 @@ function Navbar({ className }: { className?: string }) {
             <ProductItem
               title="Algochurn"
               href="https://algochurn.com"
-              // src="https://assets.aceternity.com/demos/algochurn.webp"
               description="Prepare for tech interviews like never before."
             />
             <ProductItem
               title="Tailwind Master Kit"
               href="https://tailwindmasterkit.com"
-              // src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
               description="Production ready Tailwind css components for your next project"
             />
             <ProductItem
               title="Moonbeam"
               href="https://gomoonbeam.com"
-              // src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
               description="Never write from scratch again. Go from idea to blog in minutes."
             />
             <ProductItem
               title="Rogue"
               href="https://userogue.com"
-              //src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
               description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
             />
           </div>
