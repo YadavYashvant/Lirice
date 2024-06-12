494342b2d49f29d7435f129ba5243175c3f8723b
YadavYashvant <yashvantyadav855@gmail.com>
2024-06-12 23:02:20 +0530

    user api works now!!

--- server/src/main/java/com/yashvant/lirice/models/Post.java
+++ server/src/main/java/com/yashvant/lirice/models/Post.java
@@ -1,4 +1,26 @@
 package com.yashvant.lirice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int Id;
+    private String title;
+    private String body;
+    private String category;
+    private String imageUrl;
+    private String author;
 }
