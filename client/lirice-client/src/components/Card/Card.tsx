bdbe47c561d7855d97cd756e696aaf1afe56f769
Yashvant Yadav <113130559+YadavYashvant@users.noreply.github.com>
2024-07-14 23:11:09 +0530

    Update Card.tsx

--- client/lirice-client/src/components/Card/Card.tsx
+++ client/lirice-client/src/components/Card/Card.tsx
@@ -10,9 +10,9 @@ const PostCard: React.FC<CardProps> = ({ title, description }) =>{
     return(
         <Card className="py-4 my-10 mx-10 lg:mx-52">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <p className="text-tiny uppercase font-bold">Daily Mix</p>
-        <small className="text-default-500">12 Tracks</small>
-        <h4 className="font-bold text-large">Frontend Radio</h4>
+        <p className="text-tiny uppercase font-bold">Config Header</p>
+        <small className="text-default-500">config description</small>
+        <h4 className="font-bold text-large">Hyprland, AGS, KDE, StarShip and more . . . </h4>
       </CardHeader>
       <CardBody className="overflow-visible py-2">
         <Image
