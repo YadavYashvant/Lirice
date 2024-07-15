a8fd91a368c320fdb4360eda5a55daf4dbd5307c
YadavYashvant <yashvantyadav855@gmail.com>
2024-07-16 00:09:26 +0530

    feat: Update Card component to lazy load images

--- client/lirice-client/src/components/Card/Card.tsx
+++ client/lirice-client/src/components/Card/Card.tsx
@@ -8,28 +8,28 @@ interface CardProps {
 
 const PostCard: React.FC<CardProps> = ({ title, description }) => {
     return (
-        <Card className="py-4 my-10 mx-10 lg:mx-52">
+        <Card className="py-4 my-10 mx-10 lg:mx-42 lg:my-32">
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                 <p className="text-tiny uppercase font-bold">Config Header</p>
                 <small className="text-default-500">config description</small>
                 <h4 className="font-bold text-large">Hyprland, AGS, KDE, StarShip and more . . . </h4>
             </CardHeader>
-            <CardBody className="overflow-visible py-2">
-            <Image
-      isBlurred
-      width={500}
-      src="/myarch.png"
-      alt="POST image 1"
-      className="m-5"
-    />
+            <CardBody className="overflow-visible py-2 w-full">
+              <Image
+              isBlurred
+              // width={500}
+              src="/myarch.png"
+              alt="POST image 1"
+              className="m-5 w-[50%]"
+              />
 
-<Image
-      isBlurred
-      width={500}
-      src="/agshypr.png"
-      alt="POST image 2"
-      className="m-5"
-    />
+              <Image
+              isBlurred
+              // width={500}
+              src="/agshypr.png"
+              alt="POST image 2"
+              className="m-5 w-[50%]"
+              />
 
             </CardBody>
         </Card>
