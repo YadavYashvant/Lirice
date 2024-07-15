7ae601ecb68d3230d859c27b9123cb965f5e4eaf
YadavYashvant <yashvantyadav855@gmail.com>
2024-07-15 23:38:50 +0530

    Update Card component to include agshypr image

--- client/lirice-client/src/components/Card/Card.tsx
+++ client/lirice-client/src/components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import { Card, CardBody, CardHeader, Image } from "@nextui-org/react";
+import agshypr from "../../assets/agshypr.png";
 import React from "react";
 
 interface CardProps {
@@ -18,7 +19,7 @@ const PostCard: React.FC<CardProps> = ({ title, description }) =>{
         <Image
           alt="Card background"
           className="object-cover rounded-xl"
-          src="https://private-user-images.githubusercontent.com/97237370/325270207-406b72b6-fa38-4f0d-a6c4-4d7d5d5ddcb7.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjA5NzkxOTAsIm5iZiI6MTcyMDk3ODg5MCwicGF0aCI6Ii85NzIzNzM3MC8zMjUyNzAyMDctNDA2YjcyYjYtZmEzOC00ZjBkLWE2YzQtNGQ3ZDVkNWRkY2I3LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA3MTQlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNzE0VDE3NDEzMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTM1NzYyN2M0YzkyNzMyMzg1YTAwZDkyYzg3YTJjNzVjNDYxNzlmYTZhYTg3MDJhYmIyYzRlZmFkMjVmODgxMDgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.bIEbhjUynyArUPz7qcJi-sp9thfyxZDSRou_zEi6cPw"
+          src={agshypr}
           width={270}
         />
       </CardBody>
