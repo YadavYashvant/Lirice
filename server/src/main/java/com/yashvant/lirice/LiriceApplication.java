7388b6b7352daa7b0bbfef72ab056e1d5c4349d4
YadavYashvant <yashvantyadav855@gmail.com>
2024-07-17 23:40:22 +0530

    removing unnecessary code

--- server/src/main/java/com/yashvant/lirice/LiriceApplication.java
+++ server/src/main/java/com/yashvant/lirice/LiriceApplication.java
@@ -18,12 +18,6 @@ import java.util.List;
 @SpringBootApplication(/*exclude = {DataSourceAutoConfiguration.class}*/)
 public class LiriceApplication implements CommandLineRunner {
 
-    /*@Autowired
-    private PasswordEncoder passwordEncoder;*/
-
-    /*@Autowired
-    private RoleRepo roleRepo;*/
-
     @Autowired
     private UserRepo userRepo;
 
@@ -40,7 +34,7 @@ public class LiriceApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         UserBlog user = new UserBlog();
         user.setName("Yashvant");
-        user.setEmail("yashvantyadav855@gmail.com");
+        user.setEmail("ds7428418@gmail.com");
         user.setPassword("xyz");
         user.setAbout("I am a software developer");
 //        user.setPosts(null);
@@ -48,35 +42,4 @@ public class LiriceApplication implements CommandLineRunner {
         this.userRepo.saveAll(List.of(user));
 
     }
-
-    /*@Override
-    public void run(String... args) throws Exception {
-
-//        System.out.println(this.passwordEncoder.encode("xyz"));
-
-        try {
-
-            Role role = new Role();
-            role.setId(AppConstants.ADMIN_USER);
-            role.setName("ROLE_ADMIN");
-
-            Role role1 = new Role();
-            role1.setId(AppConstants.NORMAL_USER);
-            role1.setName("ROLE_NORMAL");
-
-            List<Role> roles = List.of(role, role1);
-
-            List<Role> result = this.roleRepo.saveAll(roles);
-
-            result.forEach(r -> {
-                System.out.println(r.getName());
-            });
-
-        } catch (Exception e) {
-            // TODO: handle exception
-            e.printStackTrace();
-        }
-
-    }*/
-
 }
---
+++ spring-sec-lirice/src/main/java/com/yashvant/springseclirice/SpringSecLiriceApplication.java
@@ -0,0 +1,13 @@
+package com.yashvant.springseclirice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecLiriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecLiriceApplication.class, args);
+    }
+
+}
