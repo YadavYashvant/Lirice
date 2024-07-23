362364b6fe1ef6d3acad621d1df1aa8cd949013e
YadavYashvant <yashvantyadav855@gmail.com>
2024-07-23 12:55:27 +0530

    implementing categories

--- spring-sec-lirice/src/main/java/com/yashvant/springseclirice/entities/Post.java
+++ spring-sec-lirice/src/main/java/com/yashvant/springseclirice/entities/Post.java
@@ -1,9 +1,6 @@
 package com.yashvant.springseclirice.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,5 +22,9 @@ public class Post {
     private String content;
 
     private String imageName;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 
 }
