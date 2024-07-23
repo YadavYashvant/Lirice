17e4c755a247686be2035afa7ffcb09fb79b2bea
YadavYashvant <yashvantyadav855@gmail.com>
2024-07-23 23:29:03 +0530

    fixing serialization error

--- spring-sec-lirice/src/main/java/com/yashvant/springseclirice/entities/Post.java
+++ spring-sec-lirice/src/main/java/com/yashvant/springseclirice/entities/Post.java
@@ -23,7 +23,7 @@ public class Post {
 
     private String imageName;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
 
