8e56e69ce9d37ed7b28b2cc748c4f910d571580c
YadavYashvant <yashvantyadav855@gmail.com>
2024-07-26 23:28:37 +0530

    fixed ui issue on smaller screens

--- client/lirice-client/src/app/page.tsx
+++ client/lirice-client/src/app/page.tsx
@@ -1,39 +1,20 @@
 
 'use client';
-import { cn } from "@/utils/cn";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useMotionValue, motion, useMotionTemplate } from "framer-motion";
-import { Button, Card, Chip, NextUIProvider } from "@nextui-org/react";
-import Image from "next/image";
+import { Chip, NextUIProvider } from "@nextui-org/react";
 import Navbar from "@/components/navbar/navbar";
 import CardList from "@/components/Card/CardList";
-import { BentoGrid } from "@/components/ui/bento-grid";
-import axios from "axios";
-import React, { useEffect } from "react";
 
 export default function Home() {
-  let mouseX = useMotionValue(0);
-  let mouseY = useMotionValue(0);
-
-  function handleMouseMove({
-    currentTarget,
-    clientX,
-    clientY,
-  }: React.MouseEvent<HTMLDivElement>) {
-    if (!currentTarget) return;
-    let { left, top } = currentTarget.getBoundingClientRect();
-
-    mouseX.set(clientX - left);
-    mouseY.set(clientY - top);
-  }
-
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
   const [data, setData] = useState<any>([]);
 
   useEffect(() => {
     fetch("https://liricebackend.onrender.com/posts")
       .then(response => response.json())
       .then(data => {
-        console.log(data);
         const formattedData = data.map((item: any) => ({
           title: item.title,
           description: item.content,
@@ -44,38 +25,49 @@ export default function Home() {
       .catch(error => console.error("Error fetching data:", error));
   }, []);
 
-    return (
-      <NextUIProvider>
-        <div
-          className="absolute h-[500vh] flex flex-col items-center bg-white dark:bg-black justify-start w-full group"
-          onMouseMove={handleMouseMove}
-        >
-          <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800 pointer-events-none" />
-  
-          <motion.div
-            className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500 absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
-            style={{
-              WebkitMaskImage: useMotionTemplate`
-                radial-gradient(
-                  200px circle at ${mouseX}px ${mouseY}px,
-                  black 0%,
-                  transparent 100%
-                )
-              `,
-              maskImage: useMotionTemplate`
-                radial-gradient(
-                  200px circle at ${mouseX}px ${mouseY}px,
-                  black 0%,
-                  transparent 100%
-                )
-              `,
-            }}
-          />
-  
-          <Navbar />
-  
-          <div className="flex flex-col items-center justify-center w-full">
-          <div className="flex gap-4 mt-40">
+  function handleMouseMove({
+    currentTarget,
+    clientX,
+    clientY,
+  }: React.MouseEvent<HTMLDivElement>) {
+    if (!currentTarget) return;
+    let { left, top } = currentTarget.getBoundingClientRect();
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+  }
+
+  return (
+    <NextUIProvider>
+      <div
+        className="absolute h-[500vh] flex flex-col items-center bg-white dark:bg-black justify-start w-full group"
+        onMouseMove={handleMouseMove}
+      >
+        <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800 pointer-events-none" />
+
+        <motion.div
+          className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500 absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
+          style={{
+            WebkitMaskImage: useMotionTemplate`
+              radial-gradient(
+                200px circle at ${mouseX}px ${mouseY}px,
+                black 0%,
+                transparent 100%
+              )
+            `,
+            maskImage: useMotionTemplate`
+              radial-gradient(
+                200px circle at ${mouseX}px ${mouseY}px,
+                black 0%,
+                transparent 100%
+              )
+            `,
+          }}
+        />
+
+        <Navbar />
+
+        <div className="flex flex-col items-center justify-center w-full max-sm:p-5">
+          <div className="flex flex-wrap gap-4 mt-40 overflow-hidden">
             <Chip color="warning" variant="solid" onClick={data}>All</Chip>
             <Chip color="warning" variant="bordered">Category 1</Chip>
             <Chip color="warning" variant="bordered">Category 2</Chip>
@@ -84,10 +76,9 @@ export default function Home() {
             <Chip color="warning" variant="bordered">Category 5</Chip>
             <Chip color="warning" variant="bordered">Category 6</Chip>
             <Chip color="warning" variant="bordered">Category 7</Chip>
-
           </div>
-          <div className="flex gap-4 mt-10">
-          <Chip color="warning" variant="bordered">Category 8</Chip>
+          <div className="flex flex-wrap gap-4 mt-10">
+            <Chip color="warning" variant="bordered">Category 8</Chip>
             <Chip color="warning" variant="bordered">Category 9</Chip>
             <Chip color="warning" variant="bordered">Category 10</Chip>
             <Chip color="warning" variant="bordered">Category 11</Chip>
@@ -95,10 +86,9 @@ export default function Home() {
             <Chip color="warning" variant="bordered">Category 13</Chip>
             <Chip color="warning" variant="dot">FOURTEEN</Chip>
           </div>
-            </div>
-          <CardList data={data} />
         </div>
-      </NextUIProvider>
-    );
-  }
-
+        <CardList data={data} />
+      </div>
+    </NextUIProvider>
+  );
+}
