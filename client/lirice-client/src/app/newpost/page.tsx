d275aa65fb0d26fa81a73b6cf3af65fd533c6d54
YadavYashvant <yashvantyadav855@gmail.com>
2024-07-28 02:39:01 +0530

    deployment works flawlessly with some loaders to make it even better 🕳️

--- client/lirice-client/src/app/newpost/page.tsx
+++ client/lirice-client/src/app/newpost/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import { NextUIProvider, Input, Button, Textarea } from '@nextui-org/react';
 import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
@@ -8,28 +8,36 @@ import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { storage } from '../../../firebaseConfig';
 import { BASE_URL } from '@/utils/baseurl';
 
-
 const NewPost: React.FC = () => {
-  const fileInputRef = React.createRef<HTMLInputElement>();
+  const fileInputRef = useRef<HTMLInputElement>(null);
   const defaultImageUrl = '/images/default-image.png';
   const [imagePreviewUrl, setImagePreviewUrl] = useState(defaultImageUrl);
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [image, setImage] = useState<File | null>(null);
+  const [loading, setLoading] = useState(false);
   let mouseX = useMotionValue(0);
   let mouseY = useMotionValue(0);
 
-  function handleMouseMove({
+  const maskImage = useMotionTemplate`
+    radial-gradient(
+      200px circle at ${mouseX}px ${mouseY}px,
+      black 0%,
+      transparent 100%
+    )
+  `;
+
+  const handleMouseMove = ({
     currentTarget,
     clientX,
     clientY,
-  }: React.MouseEvent<HTMLDivElement>) {
+  }: React.MouseEvent<HTMLDivElement>) => {
     if (!currentTarget) return;
     let { left, top } = currentTarget.getBoundingClientRect();
 
     mouseX.set(clientX - left);
     mouseY.set(clientY - top);
-  }
+  };
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files ? e.target.files[0] : null;
@@ -48,46 +56,116 @@ const NewPost: React.FC = () => {
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
+    setLoading(true);
     try {
       const postResponse = await axios.post(`${BASE_URL}/post`, {
         title,
         content,
       });
-  
-      // console.log('Post response:', postResponse);
-  
+
       const postId = postResponse.data.id;
       if (!postId) {
         throw new Error('Post ID is undefined');
       }
-  
+
       console.log('Post created with ID:', postId);
-  
+
       if (image) {
         const storageRef = ref(storage, `images/${image.name}`);
         await uploadBytes(storageRef, image);
         const imageUrl = await getDownloadURL(storageRef);
         console.log('Image uploaded to Firebase:', imageUrl);
-      
+
         const formData = new FormData();
         formData.append('image', imageUrl);
-      
+
         await axios.post(`${BASE_URL}/post/image/upload/${postId}`, formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
         });
-      
+
         console.log('Image uploaded and linked to post');
       }
-  
+
       setTitle('');
       setContent('');
       setImage(null);
     } catch (error) {
       console.error('Error creating post:', error);
+    } finally {
+      setLoading(false);
     }
   };
+
+  if (loading) {
+    return (
+      <NextUIProvider>
+        <div
+          className="absolute h-[400vh] flex flex-col items-center bg-white dark:bg-black justify-start w-full group"
+          onMouseMove={handleMouseMove}
+        >
+          <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800 pointer-events-none" />
+          <motion.div
+            className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500 absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
+            style={{
+              WebkitMaskImage: maskImage,
+              maskImage: maskImage,
+            }}
+          />
+          <form onSubmit={handleSubmit} className="bg-inherit p-6 rounded shadow-md min-w-full h-full max-w-md mx-24 flex items-center justify-start flex-col">
+            <Card className="min-w-full mb-12 p-10">
+              <CardBody>
+                <h1 className='text-7xl text-center'>CREATE A NEW POST 🚀</h1>
+              </CardBody>
+              <Divider />
+              <CardFooter className="flex items-center justify-center">
+                <Link isExternal showAnchorIcon href="https://github.com/YadavYashvant/Lirice">
+                  See some examples to get an idea
+                </Link>
+              </CardFooter>
+            </Card>
+            <div className="mb-4 w-[95%]">
+              <Input
+                fullWidth
+                label="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-4 w-[95%]">
+              <Textarea
+                fullWidth
+                minRows={20}
+                label="Content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-4 w-[95%]">
+              <Card onClick={() => fileInputRef.current?.click()} className="cursor-pointer">
+                <CardHeader>
+                  <input
+                    type="file"
+                    ref={fileInputRef}
+                    onChange={handleImageChange}
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Image src={imagePreviewUrl} alt="Selected or default image" />
+                </CardBody>
+              </Card>
+            </div>
+            <Button className='w-[80%] p-7 my-7' type="submit" color="primary" fullWidth isLoading>
+              <h2 className='text-2xl'>POST</h2>
+            </Button>
+          </form>
+        </div>
+      </NextUIProvider>
+    );
+  }
 
   return (
     <NextUIProvider>
@@ -96,27 +174,13 @@ const NewPost: React.FC = () => {
         onMouseMove={handleMouseMove}
       >
         <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800 pointer-events-none" />
-
         <motion.div
           className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500 absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
           style={{
-            WebkitMaskImage: useMotionTemplate`
-              radial-gradient(
-                200px circle at ${mouseX}px ${mouseY}px,
-                black 0%,
-                transparent 100%
-              )
-            `,
-            maskImage: useMotionTemplate`
-              radial-gradient(
-                200px circle at ${mouseX}px ${mouseY}px,
-                black 0%,
-                transparent 100%
-              )
-            `,
+            WebkitMaskImage: maskImage,
+            maskImage: maskImage,
           }}
         />
-
         <form onSubmit={handleSubmit} className="bg-inherit p-6 rounded shadow-md min-w-full h-full max-w-md mx-24 flex items-center justify-start flex-col">
           <Card className="min-w-full mb-12 p-10">
             <CardBody>
--- client/lirice-client/src/app/page.tsx
+++ client/lirice-client/src/app/page.tsx
@@ -6,11 +6,13 @@ import { Chip, NextUIProvider } from "@nextui-org/react";
 import Navbar from "@/components/navbar/navbar";
 import CardList from "@/components/Card/CardList";
 import { BASE_URL } from "@/utils/baseurl";
+import Loader from "@/components/Loader";
 
 export default function Home() {
   const mouseX = useMotionValue(0);
   const mouseY = useMotionValue(0);
   const [data, setData] = useState<any>([]);
+  const [loading, setLoading] = useState(true);
 
   
 
@@ -26,44 +28,10 @@ export default function Home() {
         }));
         setData(formattedData);
       })
-      .catch(error => console.error("Error fetching data:", error));
+      .catch(error => console.error("Error fetching data:", error))
+      .finally(() => setLoading(false));
 
       console.log("data that we are getting after /posts get request: ",data);
-
-    /*
-    const storedData = localStorage.getItem("postsData");
-    if (storedData) {
-      // setData(JSON.parse(storedData));
-
-      fetch(`${BASE_URL}/posts`)
-        .then(response => response.json())
-        .then(data => {
-          const formattedData = data.map((item: any) => ({
-            title: item.title,
-            description: item.content,
-            image: item.imageName,
-          }));
-          setData(formattedData);
-          localStorage.setItem("postsData", JSON.stringify(formattedData));
-          console.log("",formattedData);
-        })
-        .catch(error => console.error("Error fetching data:", error));
-
-    } else {
-      fetch(`${BASE_URL}/posts`)
-        .then(response => response.json())
-        .then(data => {
-          const formattedData = data.map((item: any) => ({
-            title: item.title,
-            description: item.content,
-            image: item.imageName,
-          }));
-          setData(formattedData);
-          localStorage.setItem("postsData", JSON.stringify(formattedData));
-        })
-        .catch(error => console.error("Error fetching data:", error));
-    }
-        */
   }, []);
 
   function handleMouseMove({
@@ -78,6 +46,64 @@ export default function Home() {
   }
 
   console.log(data);
+
+  if(loading) {
+    return(
+      <NextUIProvider>
+      <div
+        className="absolute h-[500vh] flex flex-col items-center bg-white dark:bg-black justify-start w-full group"
+        onMouseMove={handleMouseMove}
+      >
+        <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800 pointer-events-none" />
+
+        <motion.div
+          className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500 absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
+          style={{
+            WebkitMaskImage: useMotionTemplate`
+              radial-gradient(
+                200px circle at ${mouseX}px ${mouseY}px,
+                black 0%,
+                transparent 100%
+              )
+            `,
+            maskImage: useMotionTemplate`
+              radial-gradient(
+                200px circle at ${mouseX}px ${mouseY}px,
+                black 0%,
+                transparent 100%
+              )
+            `,
+          }}
+        />
+
+        <Navbar />
+
+        <div className="flex flex-col items-center justify-center w-full">
+          <div className="flex flex-wrap gap-4 mt-40 overflow-hidden">
+            <Chip color="warning" variant="solid" onClick={data}>All</Chip>
+            <Chip color="warning" variant="bordered">Category 1</Chip>
+            <Chip color="warning" variant="bordered">Category 2</Chip>
+            <Chip color="warning" variant="bordered">Category 3</Chip>
+            <Chip color="warning" variant="bordered">Category 4</Chip>
+            <Chip color="warning" variant="bordered">Category 5</Chip>
+            <Chip color="warning" variant="bordered">Category 6</Chip>
+            <Chip color="warning" variant="bordered">Category 7</Chip>
+          </div>
+          <div className="flex flex-wrap gap-4 mt-10">
+            <Chip color="warning" variant="bordered">Category 8</Chip>
+            <Chip color="warning" variant="bordered">Category 9</Chip>
+            <Chip color="warning" variant="bordered">Category 10</Chip>
+            <Chip color="warning" variant="bordered">Category 11</Chip>
+            <Chip color="warning" variant="bordered">Category 12</Chip>
+            <Chip color="warning" variant="bordered">Category 13</Chip>
+            <Chip color="warning" variant="dot">FOURTEEN</Chip>
+          </div>
+        </div>
+        <Loader />
+      </div>
+    </NextUIProvider>
+    )
+  }
 
   return (
     <NextUIProvider>
