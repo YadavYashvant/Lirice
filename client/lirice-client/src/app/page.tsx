fc22b50fc09e2e5acd1b46064affc9631d0e917c
YadavYashvant <yashvantyadav855@gmail.com>
2024-08-12 22:59:05 +0530

    making authentication frontend

--- client/lirice-client/src/app/page.tsx
+++ client/lirice-client/src/app/page.tsx
@@ -81,12 +81,12 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center w-full max-sm:mx-10">
 
           <div className="flex flex-wrap gap-4 mt-40 overflow-hidden">
-            <Button radius="full">
+            <Button variant="shadow" color="success" radius="full" className='p-6'>
               <a href='/auth' className="text-lg font-bold">SignIn or SignUp</a>
             </Button>
           </div>
 
-          <div className="flex flex-wrap gap-4 mt-40 overflow-hidden">
+          <div className="flex flex-wrap gap-4 mt-10 overflow-hidden">
             <Chip color="warning" variant="solid" onClick={data}>All</Chip>
             <Chip color="warning" variant="bordered">Category 1</Chip>
             <Chip color="warning" variant="bordered">Category 2</Chip>
