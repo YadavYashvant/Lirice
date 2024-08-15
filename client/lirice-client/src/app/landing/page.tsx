b4ff415a1e21b2eb934c0970b529255b2edf7536
YadavYashvant <yashvantyadav855@gmail.com>
2024-08-15 23:08:56 +0530

    fixing errors

---
+++ client/lirice-client/src/app/landing/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from "react";
+import { useState } from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <h1>Landing Page</h1>
+        </div>
+    );
+}
+
+export default LandingPage;
