9600afb697a645753bdc6e45fed4361137aa5ab7
YadavYashvant <yashvantyadav855@gmail.com>
2024-08-16 23:22:23 +0530

    making authentication frontend

--- client/lirice-client/src/app/landing/page.tsx
+++ client/lirice-client/src/app/landing/page.tsx
@@ -1,11 +1,21 @@
 'use client';
 import React from "react";
+import {Image} from "@nextui-org/react";
 import { useState } from 'react';
 
 const LandingPage = () => {
     return (
         <div>
             <h1 className='text-7xl text-center'>Welcome to Lirice</h1>
+
+            <Image
+                isBlurred
+                className='w-full'
+                src="https://nextui-docs-v2.vercel.app/images/album-cover.png"
+                alt="NextUI Album Cover"
+                // className="m-5"
+            />
+
         </div>
     );
 }
