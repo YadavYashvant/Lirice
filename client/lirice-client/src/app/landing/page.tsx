d9eb99c865ae7d1ba452ba25154716755dbeda7c
YadavYashvant <yashvantyadav855@gmail.com>
2024-08-19 23:45:17 +0530

    making authentication frontend

--- client/lirice-client/src/app/landing/page.tsx
+++ client/lirice-client/src/app/landing/page.tsx
@@ -6,12 +6,12 @@ import { useState } from 'react';
 const LandingPage = () => {
     return (
         <div className='relative overflow-x-hidden'>
-            <div className='absolute inset-0 flex items-center justify-center z-10'>
-                <h1 className='text-9xl text-center font-extrabold text-white'>Welcome to Lirice</h1>
+            <div className='absolute top-0 left-0 right-0 flex items-end justify-center z-10'>
+                <h1 className='text-9xl text-center font-extrabold text-white mt-14'>Welcome to Lirice</h1>
             </div>
             <Image
                 isBlurred
-                className='w-[95%] m-10 overflow-x-hidden'
+                className='w-[95%] m-10 overflow-x-hidden z-0'
                 src="https://images.unsplash.com/photo-1600354279787-0a726615ef44"
                 alt="Lirice landing page image"
             />
