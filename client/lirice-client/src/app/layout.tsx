3914def3448ba524f7100dce0fa2996a7bb9c72f
YadavYashvant <yashvantyadav855@gmail.com>
2024-08-20 23:44:18 +0530

    making authentication frontend

--- client/lirice-client/src/app/layout.tsx
+++ client/lirice-client/src/app/layout.tsx
@@ -2,7 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Poppins } from 'next/font/google';
 
+const poppins = Poppins({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+    display: "swap",
+});
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
