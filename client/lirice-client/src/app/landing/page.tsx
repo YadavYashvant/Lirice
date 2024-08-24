ffad051b3f775b2964e5ccb0f0be5b710fa4fc8a
YadavYashvant <yashvantyadav855@gmail.com>
2024-08-24 23:43:35 +0530

    making authentication frontend

--- client/lirice-client/src/app/landing/page.tsx
+++ client/lirice-client/src/app/landing/page.tsx
@@ -6,20 +6,22 @@ import { useState } from 'react';
 const LandingPage = () => {
     return (
         <div className='relative overflow-x-hidden'>
-            <div className='absolute inset-0 bg-black opacity-50 z-0'></div>
-            <div className='absolute top-0 left-0 right-0 flex flex-col items-center justify-center z-10'>
-                <h1 className='text-9xl text-center font-extrabold text-white mt-14'>Welcome to Lirice</h1>
-                <div className='text-2xl text-center font-semibold text-white mt-4'>
-                    Share linux desktop configs in one place 🖥️
-                </div>
-            </div>
-            <div className='flex items-center justify-center w-full h-full'>
+            <div className='relative w-full'>
                 <Image
                     isBlurred
-                    className='w-1/2 m-10 overflow-x-hidden z-0'
+                    className='w-full h-full object-cover'
                     src="https://images.unsplash.com/photo-1600354279787-0a726615ef44"
                     alt="Lirice landing page image"
                 />
+                <div className='absolute inset-0 bg-black opacity-50'></div>
+                <div className='absolute inset-0 flex flex-col items-center justify-center z-10'>
+                    <h1 className='text-9xl text-center font-extrabold text-white mt-14'>Welcome to Lirice</h1>
+                    <div className='text-2xl text-center font-semibold text-white mt-4'>
+                        Share linux desktop configs in one place 🖥️
+                    </div>
+                </div>
+            </div>
+            <div className='flex items-center justify-center w-full h-full'>
                 <Card className='w-1/2 p-10 z-10'>
                     <h1 className='text-5xl text-center font-extrabold my-5'>Get an early access</h1>
                     <Input
