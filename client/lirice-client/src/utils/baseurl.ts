53797d52312e3bf932589acfc1f77e0d535165d6
Yashvant Yadav <113130559+YadavYashvant@users.noreply.github.com>
2024-10-20 16:40:22 +0530

    Update baseurl.ts

--- client/lirice-client/src/utils/baseurl.ts
+++ client/lirice-client/src/utils/baseurl.ts
@@ -1,2 +1,3 @@
 // export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL = 'https://liricebackend.onrender.com';
+// export const BASE_URL = 'https://liricebackend.onrender.com';
+export const BASE_URL = 'https://orange-space-robot-qjggrw6p694fxqg5-8080.app.github.dev';
